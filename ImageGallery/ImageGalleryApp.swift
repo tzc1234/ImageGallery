e23f6dbd75c09773f79bd8ea5bd398ef1e7c4836
tzc1234 <peopli9997@gmail.com>
2022-08-18 11:17:26 +0100

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Tsz-Lung on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
